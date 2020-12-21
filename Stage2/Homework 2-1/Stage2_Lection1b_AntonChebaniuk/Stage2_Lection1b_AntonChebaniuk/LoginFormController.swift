3e5fe0933c5b5db1d481d243d5c5207e649261f4
Anton Chebaniuk <achebanuk@me.com>
2020-12-21 23:30:59 +0200

    Homework 2-1 is done

---
+++ Stage2/Homework 2-1/Stage2_Lection1b_AntonChebaniuk/Stage2_Lection1b_AntonChebaniuk/LoginFormController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginFormController.swift
+//  Stage2_Lection1b_AntonChebaniuk
+//
+//  Created by Anton Chebaniuk on 21.12.2020.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
